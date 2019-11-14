1742bceab09e7cb0403090aee36530e91e9d5bfa
Florence Delattre <flo-qumal@hotmail.fr>
2019-11-14 16:03:41 +0100

    almost finished robot-name

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 public class Robot
 {
-    public string Name
+
+    // Possibilité de random sur une regex ?
+
+    public Random random = new Random();
+    public HashSet<string> usedNames = new HashSet<string>();
+    public string Name { get; private set; } = null;
+
+    public Robot()
     {
-        get
+        UpdateName();
+    }
+
+    public string BeginningName()
+    {
+        string beginningName = new StringBuilder()
+                            .Append(((char)('A' + random.Next(0, 25))).ToString())
+                            .Append(((char)('A' + random.Next(0, 25))).ToString())
+                            .ToString();
+
+        return beginningName;
+    }
+
+    public string EndingName()
+    {
+        string endingName = new StringBuilder()
+                            .Append(random.Next(0, 9).ToString())
+                            .Append(random.Next(0, 9).ToString())
+                            .Append(random.Next(0, 9).ToString())
+                            .ToString();
+
+        return endingName;
+    }
+
+    public void UpdateName()
+    {
+        var name = new StringBuilder().Append(BeginningName()).Append(EndingName()).ToString();
+
+        while (usedNames.Contains(name))
         {
-            throw new NotImplementedException("You need to implement this function.");
+            name = new StringBuilder()
+                .Append(BeginningName())
+                .Append(EndingName())
+                .ToString();
         }
+
+        Name = name;
+        usedNames.Add(Name);
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        UpdateName();
     }
 }
